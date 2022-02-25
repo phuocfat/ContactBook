553a338266024905cece076aa374db30df460a75
phuocfat <72081997+phuocfat@users.noreply.github.com>
2022-02-25 11:07:40 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const { BadRequestError, errorHandler } = require("./app/errors");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const setupContactRoutes = require("./app/rotues/contact.routes");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 setupContactRoutes(app);
+
+app.use((req, res, next) => {
+  next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  errorHandler.handleError(err, res);
+});
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
