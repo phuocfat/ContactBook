fd9df32eea4e0981e1d9d55b91defe13145ba6dc
phuocfat <72081997+phuocfat@users.noreply.github.com>
2022-02-25 10:53:05 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const setupContactRoutes = require("./app/rotues/contact.routes");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+setupContactRoutes(app);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
