2393e4597f08513a0d2d1551c7d9ed753647bad9
phuocfat <72081997+phuocfat@users.noreply.github.com>
2022-03-03 16:30:40 +0700

    Cai dat cac handler try xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,10 @@
 const config = {
   app: {
     port: process.env.PORT || 8080,
+
   },
+  db: {
+    uri: process.env.MONGODB_URI || "mongodb+srv://Phuoc2432001:Phuoc2432001@cluster0.mo0z1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  }
 };
 module.exports = config;
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const app = require("./app.js");
 const config = require("./app/config");
+const mongoose = require("mongoose");
 
+mongoose.connect(config.db.uri)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Can not connect to the database!", err);
+    process.exit();
+  })
 const PORT = config.app.port;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}.`);
+  console.log(`Server is running on port: ${PORT}`);
 });
